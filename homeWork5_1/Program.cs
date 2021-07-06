c42edce31f164b962cdb19b5b6ff3fd6db3d089a
Aleksander Bobryshev <62813365+Trabli@users.noreply.github.com>
2021-07-06 14:49:14 +0600

    Update Program.cs

--- homeWork5_1/Program.cs
+++ homeWork5_1/Program.cs
@@ -6,19 +6,24 @@ namespace homeWork5_1
     {
         static void Main(string[] args)
         {
-            int oldLadys_inRow;
-            int time_ForOnePerson_inMinutes = 10;
-            int hour_ToWait;
-            int min_ToWait;
+           int oldWomenCount;
+            int timeFor1oldWoman;
+            int waitingTime;
+            int hourWaiting;
+            int minutesWaiting;
             
-            Console.WriteLine("Вы входите в поликлинику и видите очередь из старушек.");
-            Console.Write("Сколько старушек перед вами?: ");
-            oldLadys_inRow = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Вы заходите в больницу и видите в очереди некоторое количество старушек");
+            Console.Write("Потратив некоторе время Вы всё-же подсчитали их количество. Их оказалось: ");
 
-            hour_ToWait = (oldLadys_inRow * time_ForOnePerson_inMinutes) / 60;
-            min_ToWait = (oldLadys_inRow * time_ForOnePerson_inMinutes) % 60;
+            oldWomenCount = Convert.ToInt32(Console.ReadLine());
+            timeFor1oldWoman = 10;
 
-            Console.WriteLine("Так как на обслуживание одного человека уходит " + time_ForOnePerson_inMinutes + " минут, Вам предстоит прождать в очереди, примерно, " + hour_ToWait + " часов, " + min_ToWait + " минут.");
+            waitingTime = oldWomenCount * timeFor1oldWoman;
+            hourWaiting = waitingTime / 60;
+            minutesWaiting = waitingTime % 60;
+
+            Console.WriteLine("Так как Вы увидели, что в среднем на обследование 1 старушки уходит " + timeFor1oldWoman + " минут,");
+            Console.Write("Вы поняли, что ждать вам прийдеться " + hourWaiting + " часов " + minutesWaiting + " минут");
        }
     }
 }
